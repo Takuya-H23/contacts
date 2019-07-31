10d0392ce808e2238873c1fe294b22c6b6c0779a
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-07-31 14:14:17 -0400

    add register function

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { FaIdCardAlt } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaIdCardAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({title}) => {
+const Navbar = ({ title }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
@@ -11,19 +11,29 @@ const Navbar = ({title}) => {
         {title}
       </h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/Register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired
+};
 
 Navbar.defaultProps = {
   title: 'Contacts'
-}
+};
 
-export default Navbar
+export default Navbar;
