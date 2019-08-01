84faa0318c0977777744d69e3585b245cf31bef8
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-07-31 22:53:15 -0400

    add login logout functions and private route

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { FaIdCardAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import AuthContext from './../../context/auth/authContext';
+import { FaSignOutAlt } from 'react-icons/fa';
 
 const Navbar = ({ title }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => logout();
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href="#!" onClick={onLogout}>
+          <FaSignOutAlt />
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/Register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
@@ -11,18 +41,10 @@ const Navbar = ({ title }) => {
         {title}
       </h1>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
+        {/* <li>
           <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/Register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
+        </li> */}
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </div>
   );
