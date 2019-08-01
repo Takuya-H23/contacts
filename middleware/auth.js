cd067075da7877ab0c25252aa7a03fd4d74edc0b
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-08-01 19:04:48 -0400

    fix config

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const keys = require('./../config/keys');
 
 module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
   // Check if token exists
-  if(!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied'});
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
     // get the payload from token then make it available user property on req
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, keys.jwtSecret);
     req.user = decoded.user;
     next();
-  } catch(err) {
-    res.status(401).json({ msg: 'Token is not valid'});
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
   }
-}
+};
